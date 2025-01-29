21cdc549c322141dc9c532a33a0a41daba50e599
Byiringiro-saad <byiringirosaad@gmail.com>
2025-01-29 00:55:55 -0800

    new script to create files

--- the_creator.sh
+++ the_creator.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-mkdir students_directory
+student_Directory="students_directory"
+if [-d"$student_Directory"]; then echo "Directory '$student_Directory' already exists."
+else
+	mkdir students_directory
+fi
 cd students_directory
-touch cohort-1.txt cohort-2.txt cohort-3.txt
-echo "This is the updated script!"
+COHORTS=("cohort1.txt" "cohort2.txt" "cohort3.txt" "cohort4.txt" "cohort5.txt")
+for file in "${COHORTS[@]}"; do
+	touch "$file"
+	echo "Created file: $file"
+done
